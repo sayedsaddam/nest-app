a0c404061c942a09507c1a713d8e65c9f0680659
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-10-22 00:24:56 +0500

    created new directory inside src folder for crud application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from 'app.controller';
+import { AppService } from 'app.service';
 import { StudentModule } from 'src/student/student.module';
 import { TeacherModule } from 'src/teacher/teacher.module';
+import { UserModule } from 'src/users/user.module';
 
 @Module({
-  imports: [TeacherModule, StudentModule]
+  imports: [TeacherModule, StudentModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
